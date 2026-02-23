10d2fdff14e5a0c1591c53622017560f2c061df7
MuhammedSarihan <m.ali.mash21@gmail.com>
2026-02-23 10:54:46 +0300

    Arama Motoru
    Admin panelindeki tüm listeleme sayfalarına (Ürünler, Müşteriler, Varyasyonlar, Markalar, Kategoriler, Kampanyalar, Slider) anında filtreleme yapan JS tabanlı "Canlı Arama" (Live Search) motoru entegre edildi

--- Tasarim/Tasarim.Data/Migrations/20260223063300_slider.Designer.cs
+++ Tasarim/Tasarim.Data/Migrations/20260223063300_slider.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tasarim.Data;
 
@@ -11,9 +12,11 @@ using Tasarim.Data;
 namespace Tasarim.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20260223063300_slider")]
+    partial class slider
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Tasarim/Tasarim.Data/Migrations/20260223063300_slider.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tasarim.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class slider : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "KampanyaResmi",
+                table: "Kampanyalar",
+                type: "varchar(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldMaxLength: 150);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "KampanyaResmi",
+                table: "Kampanyalar",
+                type: "varchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
